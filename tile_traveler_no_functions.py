c9ec9d44bda7be5887e77328cef4c33c144c694f
Johann Hilmir <brotherjoi@gmail.com>
2018-09-18 09:09:43 +0000

    first commit, algorithm written

---
+++ tile_traveler_no_functions.py
@@ -0,0 +1,5 @@
+# 1. Player starts in tile 1.1
+# 2. We check where the player can move and tell him what directions he can go
+# 3. If the player inputs an invalid direction, his position does not change
+# 4. If he inputs a valid direction, we change his postition and repeat step 2
+# 5. Repeat until player reaches the winning position at 3.1
