2a6a0dec405abd90ae253eb177db9184a410abba
Johann Hilmir <brotherjoi@gmail.com>
2018-09-18 10:36:27 +0000

    Added the victory print and fixed how we handle player input

--- tile_traveler_no_functions.py
+++ tile_traveler_no_functions.py
@@ -44,28 +44,31 @@ while player_x_pos != 3 or player_y_pos != 1:
         west_bool = True
     print(".")
 
-    
-    player_direction_str = input("Direction: ")
-    player_direction_str = player_direction_str.lower()
-    if player_direction_str == "n":
-        if north_bool == True:
-            player_y_pos += 1
+    x_check, y_check = player_x_pos, player_y_pos
+
+    while player_x_pos == x_check and player_y_pos == y_check:
+        player_direction_str = input("Direction: ")
+        player_direction_str = player_direction_str.lower()
+        if player_direction_str == "n":
+            if north_bool == True:
+                player_y_pos += 1
+            else:
+                print("Not a valid direction!")
+        elif player_direction_str == "e":
+            if east_bool == True:
+                player_x_pos += 1
+            else:
+                print("Not a valid direction!")
+        elif player_direction_str == "s":
+            if south_bool == True:
+                player_y_pos -= 1
+            else:
+                print("Not a valid direction!")
+        elif player_direction_str == "w":
+            if west_bool == True:
+                player_x_pos -= 1
+            else:
+                print("Not a valid direction!")
         else:
             print("Not a valid direction!")
-    elif player_direction_str == "e":
-        if east_bool == True:
-            player_x_pos += 1
-        else:
-            print("Not a valid direction!")
-    elif player_direction_str == "s":
-        if south_bool == True:
-            player_y_pos -= 1
-        else:
-            print("Not a valid direction!")
-    elif player_direction_str == "w":
-        if west_bool == True:
-            player_x_pos -= 1
-        else:
-            print("Not a valid direction!")
-    else:
-        print("Not a valid direction!")
+print("Victory!")
